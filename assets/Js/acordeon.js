686e89196dcdaeda101812ffea830cc93196f2ad
Rafael7dev <biosraf@gmail.com>
2023-09-09 15:20:58 -0300

    commit

---
+++ assets/Js/acordeon.js
@@ -0,0 +1,16 @@
+const acordeonTriggues = document.querySelectorAll('.acordeon .trigger')
+
+acordeonTriggues.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if (isOpen) {
+            acordeon.classList.remove('open')
+        } else{
+            acordeon.classList.add('open')
+        }
+
+    })
+
+})
