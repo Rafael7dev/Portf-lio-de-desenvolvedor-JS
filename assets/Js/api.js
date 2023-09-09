217bdf0a7e0e25e058ca18c46625110c535ddacf
Rafael7dev <biosraf@gmail.com>
2023-09-09 16:59:47 -0300

    alterando api

--- assets/Js/api.js
+++ assets/Js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url ='https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url ='https://github.com/Rafael7dev/Portfolio-de-Desenvolvedor-JS/blob/hbo/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
