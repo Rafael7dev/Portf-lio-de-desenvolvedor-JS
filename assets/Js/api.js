903e01fb34349819383a42edb22b4e30257828d8
Rafael7dev <biosraf@gmail.com>
2023-09-09 17:04:04 -0300

    api

--- assets/Js/api.js
+++ assets/Js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url ='https://github.com/Rafael7dev/Portfolio-de-Desenvolvedor-JS/blob/hbo/data/profile.json';
+    const url ='https://raw.githubusercontent.com/Rafael7dev/Portfolio-de-Desenvolvedor-JS/hbo/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
