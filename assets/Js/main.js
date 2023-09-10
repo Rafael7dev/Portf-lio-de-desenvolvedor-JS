b24d65450e97739f977290ad9346c4cbb4a6111e
Rafael7dev <biosraf@gmail.com>
2023-09-09 21:39:42 -0300

    portifolio

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -2,10 +2,10 @@
 function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
-    photo.alt = profileData.name   
+    photo.alt = profileData.name
 
     const name = document.getElementById('profile.name')
-    name.innerText = profileData.name 
+    name.innerText = profileData.name
 
     const job = document.getElementById('profile.job')
     job.innerText = profileData.job
@@ -32,9 +32,22 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
-function updateLanguages(profileData){
+function updateLanguages(profileData) {
     const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(languages =>`<li>${languages}</li>`).join('')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const Portfolio = document.getElementById('profile.portfolio')
+    Portfolio.innerHTML = profileData.Portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github' : ''}> ${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+
 }
 
 
